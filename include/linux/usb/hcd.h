b36b2e7e2156e8cf84565e046945606c3f035149
bigsupersquid <0.uradork.0@gmail/com>
2013-08-06 13:05:40 -0500

    more otg patches

--- include/linux/usb/hcd.h
+++ include/linux/usb/hcd.h
@@ -379,17 +379,17 @@ extern struct usb_hcd *usb_create_shared_hcd(const struct hc_driver *driver,
 extern struct usb_hcd *usb_get_hcd(struct usb_hcd *hcd);
 extern void usb_put_hcd(struct usb_hcd *hcd);
 extern int usb_hcd_is_primary_hcd(struct usb_hcd *hcd);
-#ifdef CONFIG_USB
-extern int usb_add_hcd(struct usb_hcd *hcd,
-		unsigned int irqnum, unsigned long irqflags);
-extern void usb_remove_hcd(struct usb_hcd *hcd);
-#else
+#if !defined(CONFIG_USB)
 static inline int
 usb_add_hcd(struct usb_hcd *hcd, unsigned int irqnum, unsigned long irqflags)
 {
 	return 0;
 }
 static inline void usb_remove_hcd(struct usb_hcd *hcd) {}
+#else
+extern int usb_add_hcd(struct usb_hcd *hcd,
+		unsigned int irqnum, unsigned long irqflags);
+extern void usb_remove_hcd(struct usb_hcd *hcd);
 #endif
 
 struct platform_device;
