971c5be587cd3d568cf6bfc553b1e107726d705a
thekraven <savethestyle1@gmail.com>
2014-01-30 02:15:48 -0600

    Revert "Revert "Revert "bluetooth: jb_mr1_chocolate -> jb_2.6"""
    This reverts commit 384af29d227f68cdc183e45bc7913974c828b469.

--- include/net/bluetooth/hci.h
+++ include/net/bluetooth/hci.h
@@ -86,6 +86,7 @@ enum {
 	HCI_SERVICE_CACHE,
 	HCI_LINK_KEYS,
 	HCI_DEBUG_KEYS,
+	HCI_UNREGISTER,
 
 	HCI_RESET,
 };
@@ -119,7 +120,6 @@ enum {
 #define HCIINQUIRY	_IOR('H', 240, int)
 
 /* HCI timeouts */
-#define HCI_DISCONN_AUTH_FAILED_TIMEOUT	(10)	/* 10 ms */
 #define HCI_CONNECT_TIMEOUT	(40000)	/* 40 seconds */
 #define HCI_DISCONN_TIMEOUT	(2000)	/* 2 seconds */
 #define HCI_PAIRING_TIMEOUT	(60000)	/* 60 seconds */
@@ -410,11 +410,6 @@ struct hci_cp_read_remote_ext_features {
 
 #define HCI_OP_READ_REMOTE_VERSION	0x041d
 struct hci_cp_read_remote_version {
-	__le16   handle;
-} __packed;
-
-#define HCI_OP_READ_CLOCK_OFFSET	0x041f
-struct hci_cp_read_clock_offset {
 	__le16   handle;
 } __packed;
 
